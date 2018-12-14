5468426df8695b88e00d0527e9f47c06db9ee721
長崎　健斗 <kento@nagasakikentonoMacBook-Pro.local>
2018-12-14 18:48:55 +0900

    owl-carousel added

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,28 @@
 $(window).on('load', function() {
     $('.preloader__status').fadeOut();
-    $('.preloader').delay(350).fadeOut();
+    $('.preloader').delay(350).fadeOut(slow);
 });
+
+
+$(function(){
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 2
+            }
+        }
+    });
+  });
