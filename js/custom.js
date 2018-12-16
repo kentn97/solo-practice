ff23780dd09b2b0a24703dfdd8d854f66a110f84
長崎　健斗 <kento@nagasakikentonoMacBook-Pro.local>
2018-12-16 10:08:03 +0900

    team section completed

--- js/custom.js
+++ js/custom.js
@@ -26,3 +26,26 @@ $(function(){
         }
     });
   });
+
+  /* =========================================
+                Progress Bars
+============================================ */
+
+$(function () {
+
+    $("#progress-elements").waypoint(function () {
+
+        $(".progress-bar").each(function () {
+
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 2000);
+
+        });
+
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+    });
+
+});
